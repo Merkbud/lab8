9f4e832083259d036cf93fadd56836d23de4e9f7
Merkbud <37555605+Merkbud@users.noreply.github.com>
2020-11-25 14:06:51 -0500

    test
    oudash

---
+++ ProductCatalog_v1-master/app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
@@ -0,0 +1,28 @@
+package mgarzon.createbest.productcatalog;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class ProductTest {
+
+    @Test
+    public void checkProductName(){
+        Product aProduct = new Product("1", "DELL MONITOR", 180);
+        assertEquals("Check the name of the product", "DELL MONITOR", aProduct.getProductName());
+    }
+
+    @Test
+    public void checkProductID(){
+        Product aProduct = new Product("1", "DELL MONITOR", 180);
+        assertEquals("Check the id of the product", "1", aProduct.getId());
+    }
+
+    @Test
+    public void checkProductPrice(){
+        //this should now not fail
+        Product aProduct = new Product("1", "DELL MONITOR", 180);
+        assertEquals(180.0, 180.0, aProduct.getPrice());
+    }
+
+}
